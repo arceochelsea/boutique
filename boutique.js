d0c20560d516db629867b212bc85ddc5e34b532b
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-04-23 00:31:39 -0400

    Changed boutique name, slogan, and dropdown options

--- boutique.js
+++ boutique.js
@@ -1,17 +1,18 @@
 document.getElementById("boutiqueName").innerText = "Chelsea's Boutique";
 document.getElementById("boutiqueSlogan").innerText = "Indulge In Luxury";
 
+let pieces = ["Vintage LV Handbag", "Preloved Chanel Purse", "Hermes Belt"];
+
+let dropDownList = "<select>";
+for (let i = 0; i < pieces.length; i++) {
+    dropDownList = dropDownList + `<option value="${pieces[i]}">${pieces[i]}</option>`;
+} 
+dropDownList = dropDownList + "</select>";
+
+document.getElementById("boutiquePieces").innerHTML = dropDownList;
+
 // <select id="pieces">
 // <option value="lv">Vintage LV handbag</option>
 // <option value="chanel">Preloved Chanel purse</option>
 // <option value="hermes">Hermes belt</option>
 // </select>
-
-let pieces = ["Vintage LV Handbag", "Preloved Chanel Purse", "Hermes Belt"];
-
-let linesOfHtml = "<select>";
-for (let i = 0; i > availablepieces.length; i++) {
-    linesOfHtml + `<option value="${availablepieces[i]}">${availablepieces[i]}</option>`
-} let linesOfHtml = "</select>"
-
-document.getElementById("availablepieces")
